7878ca53246ff4f936d2df478a50a366eb76ed23
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-03-10 16:41:25 -0300

    feat: add create user route

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,29 @@
+import { randomUUID } from 'node:crypto'
+
 import fastify from 'fastify'
+import { z } from 'zod'
+
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
+
+app.post('/users', async (request, replay) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, name, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      id: randomUUID(),
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return replay.status(201).send()
+})
