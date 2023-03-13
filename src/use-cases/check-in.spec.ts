d7a5901d049084c60d8060838f4f670c03078c14
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-03-13 13:12:42 -0300

    chore: validate check in date

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,27 +1,120 @@
-import { beforeEach, describe, expect, it } from 'vitest'
+import { faker } from '@faker-js/faker'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
 
 import { CheckInUseCase } from './check-in'
 
 let checkInsRepository: InMemoryCheckInsRepository
+let gymsRepository: InMemoryGymsRepository
 let sut: CheckInUseCase
 
 describe('Check In Use Case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
-    sut = new CheckInUseCase(checkInsRepository)
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CheckInUseCase(checkInsRepository, gymsRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('should be able to check in', async () => {
+    const latitude = Number(faker.address.latitude())
+    const longitude = Number(faker.address.longitude())
+
+    const gym = await gymsRepository.create({
+      description: faker.lorem.lines(),
+      title: faker.company.name(),
+      phone: faker.phone.number(),
+      latitude,
+      longitude,
+    })
+
     const { checkIn } = await sut.execute({
-      gymId: 'gym-id',
+      gymId: gym.id,
       userId: 'user-id',
+      userLatitude: latitude,
+      userLongitude: longitude,
     })
 
     expect(checkIn).toStrictEqual({
       id: expect.any(String),
-      gym_id: 'gym-id',
+      gym_id: gym.id,
+      user_id: 'user-id',
+      created_at: expect.any(Date),
+      validated_at: null,
+    })
+  })
+
+  it('should not be able to check in twice in the same day', async () => {
+    const latitude = Number(faker.address.latitude())
+    const longitude = Number(faker.address.longitude())
+
+    const gym = await gymsRepository.create({
+      description: faker.lorem.lines(),
+      title: faker.company.name(),
+      phone: faker.phone.number(),
+      latitude,
+      longitude,
+    })
+
+    vi.setSystemTime(new Date(2023, 2, 13))
+
+    await sut.execute({
+      gymId: gym.id,
+      userId: 'user-id',
+      userLatitude: latitude,
+      userLongitude: longitude,
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: gym.id,
+        userId: 'user-id',
+        userLatitude: Number(faker.address.latitude()),
+        userLongitude: Number(faker.address.longitude()),
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+
+  it('should be able to check in on different days', async () => {
+    const latitude = Number(faker.address.latitude())
+    const longitude = Number(faker.address.longitude())
+
+    const gym = await gymsRepository.create({
+      description: faker.lorem.lines(),
+      title: faker.company.name(),
+      phone: faker.phone.number(),
+      latitude,
+      longitude,
+    })
+
+    vi.setSystemTime(new Date(2023, 2, 13))
+
+    await sut.execute({
+      gymId: gym.id,
+      userId: 'user-id',
+      userLatitude: latitude,
+      userLongitude: longitude,
+    })
+
+    vi.setSystemTime(new Date(2023, 2, 14))
+
+    const { checkIn } = await sut.execute({
+      gymId: gym.id,
+      userId: 'user-id',
+      userLatitude: latitude,
+      userLongitude: longitude,
+    })
+
+    expect(checkIn).toStrictEqual({
+      id: expect.any(String),
+      gym_id: gym.id,
       user_id: 'user-id',
       created_at: expect.any(Date),
       validated_at: null,
--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -2,10 +2,15 @@ import {
   CheckIn,
   CheckInsRepository,
 } from '@/repositories/check-ins-repository'
+import { GymsRepository } from '@/repositories/gyms-repository'
+
+import { ResourceNotFoundError } from './errors/resource-not-found'
 
 type CheckInUseCaseRequest = {
   userId: string
   gymId: string
+  userLatitude: number
+  userLongitude: number
 }
 
 type CheckInUseCaseResponse = {
@@ -13,12 +18,32 @@ type CheckInUseCaseResponse = {
 }
 
 export class CheckInUseCase {
-  constructor(private readonly checkInsRepository: CheckInsRepository) {}
+  constructor(
+    private readonly checkInsRepository: CheckInsRepository,
+    private readonly gymsRepository: GymsRepository,
+  ) {}
 
   public async execute({
     gymId,
     userId,
+    userLatitude,
+    userLongitude,
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const gym = await this.gymsRepository.findById(gymId)
+
+    if (!gym) {
+      throw new ResourceNotFoundError()
+    }
+
+    const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(
+      userId,
+      new Date(),
+    )
+
+    if (checkInOnSameDate) {
+      throw new Error('')
+    }
+
     const checkIn = await this.checkInsRepository.create({
       gym_id: gymId,
       user_id: userId,
